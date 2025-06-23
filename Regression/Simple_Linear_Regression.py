f7de5cef8e3e3aabb2e02edd2ac204fba6407d00
Jimil Luhar <122004645+Jimil-Luhar@users.noreply.github.com>
2025-06-24 01:47:43 +0530

    Create Simple_Linear_Regression.py

---
+++ Regression/Simple_Linear_Regression.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+advertising_costs = np.array([100, 200, 300, 400, 500]) #Data Set
+sales = np.array([300, 500, 600, 700, 800])
+
+mean_adv = np.mean(advertising_costs) #Mean Computation
+mean_sales = np.mean(sales)
+
+m = np.sum((advertising_costs - mean_adv) * (sales - mean_sales)) / np.sum((advertising_costs - mean_adv)**2)
+c = mean_sales - m * mean_adv #Coefficients Calculation
+
+print(f"Model: Sales = {c:.2f} + {m:.2f}*Advertising_Costs") #Finally; Model: Sales = 220.00 + 1.20*Advertising_Costs
+
+plt.scatter(advertising_costs, sales, color = 'blue')
+plt.plot(advertising_costs, c + m * advertising_costs, color = 'red')
+plt.xlabel('Advertising Costs')
+plt.ylabel('Sales')
+plt.title('Advertising Costs vs Sales')
+plt.show()
